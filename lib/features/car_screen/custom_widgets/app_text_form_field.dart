9e5b5fa4ddbe66ead04331815c8f90fe2df07729
Lemon1911 <m7moud.laymouna.ml@gmail.com>
2024-07-02 17:40:54 +0300

    -added the main photo and search bar.

---
+++ lib/features/car_screen/custom_widgets/app_text_form_field.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/colors.dart';
+
+typedef Validator = String? Function(String?);
+
+class AppTextFormField extends StatelessWidget {
+  const AppTextFormField({
+    super.key,
+    this.focusBorder,
+    this.contentPadding,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.isObscureText,
+    this.suffixIcon,
+    this.backgroundColor,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.hint,
+  });
+
+  final InputBorder? focusBorder;
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final String? hint;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final Validator? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      keyboardType: keyboardType,
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: hint,
+        errorStyle: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+        ),
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(
+              horizontal: 16.w,
+              vertical: 14.h,
+            ),
+        focusedBorder: focusBorder ??
+            OutlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorsManager.grayLight,
+                width: 1.3.w,
+              ),
+              borderRadius: BorderRadius.circular(
+                16.r,
+              ),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorsManager.grayLight,
+                width: 1.3.w,
+              ),
+              borderRadius: BorderRadius.circular(
+                16.r,
+              ),
+            ),
+        hintStyle: hintStyle ??
+            TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14.sp,
+              color: ColorsManager.grayLight,
+            ),
+        hintText: hintText,
+        suffixIcon: suffixIcon != null
+            ? Padding(
+                padding: EdgeInsets.all(
+                    9.0.h), // Adjust the padding to make the icon smaller
+                child: SizedBox(
+                  height: 12.0.h, // Adjust the size as needed
+                  width: 12.0.h, // Adjust the size as needed
+                  child: suffixIcon,
+                ),
+              )
+            : null,
+        fillColor: backgroundColor ?? Colors.white,
+        filled: true,
+      ),
+      obscureText: isObscureText ?? false,
+      style: TextStyle(
+        fontWeight: FontWeight.w400,
+        fontSize: 19.sp,
+        color: ColorsManager.grayLight,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
